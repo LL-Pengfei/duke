3cf9a8919ad7f1593c91ab8db7a74e5efc5014b0
LL-Pengfei <42534460+LL-Pengfei@users.noreply.github.com>
2019-08-28 01:02:25 +0800

    Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,31 @@
+import java.util.*;
+import java.io.*;
+
 public class Duke {
     public static void main(String[] args) {
+        //Level 0 - skeleton code
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        //Level 0 ends
+
+        //Level 1 Greet Echo Exit
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        while(true) {
+            Scanner sc = new Scanner(System.in);
+            String cmd = sc.next();
+            if (cmd.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(cmd);
+            }
+        }
+        //Level 1 ends
+
     }
 }
