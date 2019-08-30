f6ca8ddf9a7ca0b83ed14a40f5ea35ee365e55a0
LL-Pengfei <42534460+LL-Pengfei@users.noreply.github.com>
2019-08-30 10:00:31 +0800

    tut01 notes updated

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -7,7 +7,8 @@
  * please do not copy the codes/any of the code segments for any purposes.
  * Plagiarism is strictly prohibited under NUS rules and regulations.
  */
-
+//tutorial01 note: better to use getters and setters to allow modification of at_day month year time inside class event
+//a good practice to do that for oop & se
 public class Event extends Task {
     protected String at_day;
     protected String at_month;
