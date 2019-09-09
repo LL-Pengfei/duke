4bb4212fd1885d929ba74e5ca371bc4e3c9f334e
LL-Pengfei <42534460+LL-Pengfei@users.noreply.github.com>
2019-09-09 10:31:02 +0800

    a-junit, junit tsting, adding junit tests to test the behaviour of the code adding junit support to the project

---
+++ src/test/java/DukeTest.java
@@ -0,0 +1,22 @@
+//@@author LL-Pengfei
+/* Author: Zhao Pengfei (Email: zhao.pengfei@comp.nus.edu.sg)
+ * Copyright © Zhao Pengfei (Email: zhao.pengfei@comp.nus.edu.sg)
+ * AY19/20 CS2113T Module Project, Individual Component
+ * School of Computing (SoC), National University of Singapore (NUS)
+ *
+ * please do not copy the codes/any of the code segments for any purposes.
+ * Plagiarism is strictly prohibited under NUS rules and regulations.
+ */
+//DukeTest.java
+//added Junit support to the project
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DukeTest {
+    @Test
+    public void dummyTest(){
+        assertEquals(2,2);
+    }
+}
